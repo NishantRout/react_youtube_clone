9551d998684799448f879f7259a9b9154d59a2f1
NishantRout <nishantnarayanrout@gmail.com>
2021-07-22 22:36:59 +0530

    Sidebar and Header updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <MenuIcon />
+        <MenuIcon className="header__icon" />
         <img
           className="header__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
