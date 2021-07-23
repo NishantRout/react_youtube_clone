d71c173132cd358f9ebb88447b3bc4e54b998738
NishantRout <nishantnarayanrout@gmail.com>
2021-07-23 17:49:01 +0530

    Recommended Videos Updates

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -45,6 +45,47 @@ function RecommendedVideos() {
       image:
         "https://i.ytimg.com/vi/B-zO3k64wig/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAFMD18Yhkv0qC2O-Zsb7M1qtEVZg",
     },
+    {
+      title:
+        "Marvel Studios’ Shang-Chi and the Legend of the Ten Rings | Official Trailer",
+      channel: "Marvel Entertainment",
+      views: "13M views",
+      timestamp: "4 weeks ago",
+      channelImage:
+        "https://yt3.ggpht.com/ytc/AKedOLSYVU0j5KAeEwuDpIkgO4LY3y-jHHwaaDMkMTdlDrA=s68-c-k-c0x00ffffff-no-rj",
+      image:
+        "https://i.ytimg.com/vi/8YjFbMbfXaQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBTypF1yTn0EcW37auHCj8DnvkgPA",
+    },
+    {
+      title: "Switzerland in 8K ULTRA HD HDR - Heaven of Earth (60 FPS)",
+      channel: "8K World",
+      views: "12M views",
+      timestamp: "8 months ago",
+      channelImage:
+        "https://yt3.ggpht.com/ytc/AKedOLSS1lyM1dY-UqjVb023wncf64ON14K9N6zQwSVH=s68-c-k-c0x00ffffff-no-rj",
+      image:
+        "https://i.ytimg.com/vi/linlz7-Pnvw/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAuKQrT0GfkMk-xpSmIWd3fvg0MZg",
+    },
+    {
+      title: "Tokyo Olympics opening ceremony to begin later - BBC News",
+      channel: "BBC News",
+      views: "17K views",
+      timestamp: "3 hours ago",
+      channelImage:
+        "https://yt3.ggpht.com/ytc/AKedOLSJf_PYHF9czwJ0c99ARvsOkYLzoUGXSVhvOvlAkoc=s68-c-k-c0x00ffffff-no-rj",
+      image:
+        "https://i.ytimg.com/vi/PrK14PquHR0/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDif-9wkkl-s6gg2fdVGTeMTdg6pg",
+    },
+    {
+      title: "THIS is the FUTURE of Apple!",
+      channel: "ZONEofTECH",
+      views: "197K views",
+      timestamp: "4 months ago",
+      channelImage:
+        "https://yt3.ggpht.com/ytc/AKedOLQdetXnECCYM6GInT1NfnCgT-ln3I9ffg98lWRswg=s68-c-k-c0x00ffffff-no-rj",
+      image:
+        "https://i.ytimg.com/vi/LHGxVKytpJE/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA9DtoeLiv3r1WSbMSUtmVTO2PU2w",
+    },
   ]);
   return (
     <div className="recommendedVideos">
@@ -63,6 +104,7 @@ function RecommendedVideos() {
           )
         )}
       </div>
+      <hr />
     </div>
   );
 }
